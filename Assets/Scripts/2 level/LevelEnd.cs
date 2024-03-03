98e55a970b5c95270a8f231ff9684b853c95141f
Mikhalik1 <zemgo1991@gmail.com>
2024-03-03 23:17:31 +0500

    sound

--- Assets/Scripts/2 level/LevelEnd.cs
+++ Assets/Scripts/2 level/LevelEnd.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class LevelEnd : MonoBehaviour
 {
     [SerializeField] private NextLevel _nextLevel;
+    [SerializeField] private AudioSource _audioSource;
 
     private bool _hasPlayer;
+    
 
     private void Update()
     {
         if (_hasPlayer == true && Input.GetKeyDown(KeyCode.E))
         {
-            _nextLevel.ChangeLevel();
+            StartCoroutine(Soundplay());
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         _hasPlayer = true;
+    }
+
+    IEnumerator Soundplay()
+    {
+        _audioSource.Play();
+        yield return new WaitForSeconds(2);
+        _nextLevel.ChangeLevel();
     }
 }
